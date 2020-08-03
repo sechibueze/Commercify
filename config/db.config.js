f892254bda8392f2c7cd1978a5d55ea992c021f9
Samuel Chibueze <lightworthng@gmail.com>
2020-08-03 09:18:02 +0100

    add server-api login endpoint

--- config/db.config.js
+++ config/db.config.js
@@ -8,10 +8,11 @@ const dbConnection = _ => {
   const options = { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
 
   console.log('process.env.TEST_MONGODBURI', process.env.TEST_MONGODBURI)
+  // console.log('process.env.TEST_MONGODBURI', process.env.TEST_MONGODBURI)
   console.log('test URI', testURI)
-  console.log('node env', process.env.NODE_ENV)
+  console.log('uri', uri)
 
-  
+
   mongoose.connect(uri, options,  err => {
     if (err) {
       return console.log('failed to connect to DB ', err);
