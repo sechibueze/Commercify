d2b0bfb64c369e5e3ee997a51252d7b5acdd4346
Samuel Chibueze <lightworthng@gmail.com>
2020-08-03 08:40:17 +0100

    add server-api login endpoint

--- test/signup.spec.js
+++ test/signup.spec.js
@@ -4,7 +4,8 @@ const expect = require('chai').expect;
 
 const app = require('../app');
 const request = supertest(app);
-
+console.log('NODE env test', process.env.NODE_ENV)
+console.log('Test DB URI', process.env.TEST_MONGODBURI)
 const User = require('../models/User');
 let users = [
   {
